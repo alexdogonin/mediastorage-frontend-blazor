037a266a3c98e211e3f0d4b1193e5fe8aba3bc2b
Alexey Dogonin <alex.dogonin@gmail.com>
2022-06-27 19:49:33 +0300

    view media

--- Program.cs
+++ Program.cs
@@ -16,10 +16,17 @@ namespace mediastorage
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
+            var host = builder.Configuration.GetValue<string>("host", "localhost");
+            var port = builder.Configuration.GetValue<int>("port", 3000);
 
-            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            var uri = new UriBuilder("http", host, port).Uri;
+
+            // builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = uri });
 
             await builder.Build().RunAsync();
         }
     }
+
+    public class Api: HttpClient{}
 }
